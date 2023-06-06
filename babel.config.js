9c8166c32be822587abbe0bf714dc68557885233
thanhlam0822 <benzamano123@gmail.com>
2023-06-06 23:12:40 +0700

    done menu header

--- babel.config.js
+++ babel.config.js
@@ -2,12 +2,5 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
-  rules: {
-    'selector-pseudo-class-no-unknown': [
-      true,
-      {
-        ignorePseudoClasses: ['deep'],
-      },
-    ],
-  }
+
 }
